2c5f6a06bc613a996ce813c20bf903fd42bc9066
Mordechai <mordwein77@gmail.com>
2022-10-30 22:51:04 -0400

    commit tree 21826b4a5c74739d46e618ccb7c2f520068457a1

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+     customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+ const leastFavoriteCustomer = "hello"
+ 
+ function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "goodbye"
+ }
+
+//  var customerName = "bob"
+
+// function upperCaseCustomerName(){
+// return customerName = "BOB"
+// }
+
+// function setBestCustomer(){
+//     return bestCustomer = 'not bob'
+// }
+
+// function overwriteBestCustomer(){
+//     return bestCustomer = 'maybe bob'
+// }
+
+// const leastFavoriteCustomer = "hello"
+
+// function changeLeastFavoriteCustomer(){
+//     return leastFavoriteCustomer = "goodbye"
+// }
